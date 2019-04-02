f689373dd03e469d41c5ca7733742e8a1ad2de4b
Marcos Vissio <marcos.vissio@gmail.com>
2019-04-02 15:46:46 -0300

    guard y page home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {NoPageFoundComponent} from './shared/no-page-found/no-page-found.component';
+import {PagesComponent} from './pages/pages.component';
+import {LoginGuardGuard} from './services/guards/login-guard.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '**',  component: NoPageFoundComponent}
+  {
+    path: '',
+    component: PagesComponent,
+    canActivate: [ LoginGuardGuard ]
+  }
+  // },
+  // {path: '**',  component: NoPageFoundComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PagesRoutingModule} from './pages-routing.module';
+import {HomeComponent} from './home/home.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {FooterComponent} from './footer/footer.component';
+import { PagesComponent } from './pages.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule
+  ]
+})
+export class PagesModule { }
