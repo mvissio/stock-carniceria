8a4a51712d35b6eddc37731b60c3cd562b13c95e
Marcos Vissio <marcos.vissio@gmail.com>
2019-04-06 10:10:02 -0300

    Assets

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { AccoutSettingsComponent } from './accout-settings/accout-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+
+
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBars' } },
+            { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+            { path: 'account-settings', component: AccoutSettingsComponent, data: { titulo: 'Ajustes de Tema' } },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+        ]
+    }
+];
+
+
+export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
