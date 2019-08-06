935c78f81623bb3c69ec4df5691fa720d8b57fd7
dariogonzalez <dgonzalez@aconcaguasf.com.ar>
2019-08-06 20:36:12 -0300

    fix label error and update config prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment, ICustomEnvironment } from "./config";
 
 export const prodEnvironment: ICustomEnvironment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/StockCustom',
   apiVersion: ''
 };
 
