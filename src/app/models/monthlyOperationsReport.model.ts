105cdf1d9033a870fcb3baed3d99c7312bcfb2b7
jackmedrano <jack_medrano@hotmail.com>
2019-09-28 18:15:46 -0300

    agrego cant por operaciones

--- src/app/models/monthlyOperationsReport.model.ts
+++ src/app/models/monthlyOperationsReport.model.ts
@@ -1,10 +1,15 @@
 export class MonthlyOperationsReport{
 totalSale : number;
 totalBuy : number;
+totalCountSale : number;
+totalCountBuy : number;
 
-constructor(totalSale:number,totalBuy : number){
+constructor(totalSale:number,totalBuy : number,totalCountSale : number,
+    totalCountBuy : number){
     this.totalSale = totalSale;
     this.totalBuy = totalBuy;
+    this.totalCountSale = totalCountSale;
+    this.totalCountBuy = totalCountBuy;
 }
 
 
